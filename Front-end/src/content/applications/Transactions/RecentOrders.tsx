6a6e559226581b0d265f49e16212e42d5fda0cf9
tuanhoang-hub <s3817872@rmit.edu.vn>
2023-05-26 10:57:34 +0700

    update api

--- Front-end/src/content/applications/Transactions/RecentOrders.tsx
+++ Front-end/src/content/applications/Transactions/RecentOrders.tsx
@@ -5,8 +5,6 @@ import { useEffect, useState } from 'react';
 import { getRequest } from 'src/util/axiosInstance';
 import { Product } from 'src/models/product';
 
-
-
 function RecentOrders() {
   const [products, setProduct] = useState<Product[]>([]);
   useEffect(() => {
@@ -15,12 +13,12 @@ function RecentOrders() {
   const getProducts = async () => {
     try {
       const request = await getRequest({
-        url: `http://127.0.0.1:5000/products`
+        url: `http://localhost:5000/products`
       });
       const products = request.data;
       setProduct(products);
     } catch (error) {
-      console.log("No products was found here");
+      console.log('No products was found here');
     }
   };
 
